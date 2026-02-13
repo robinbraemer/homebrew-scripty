1ebdd18c278587675d178b388778a9d4f2742659
Robin <robin.braemer@web.de>
2026-02-13 16:24:34 +0100

    Scripty Desktop 0.1.0
    Update sha256 and download URL to GitHub releases.

--- Casks/scripty-desktop.rb
+++ Casks/scripty-desktop.rb
@@ -1,8 +1,8 @@
 cask "scripty-desktop" do
   version "0.1.0"
-  sha256 "PLACEHOLDER"
+  sha256 "91a284952e8be672fe184c585abc1e33837b18cd964e5a460da9ea89c1b23d86"
 
-  url "https://codeberg.org/robinbraemer/scripty/releases/download/v#{version}/Scripty_#{version}_arm64.dmg"
+  url "https://github.com/robinbraemer/homebrew-scripty/releases/download/v#{version}/Scripty_#{version}_aarch64.dmg"
   name "Scripty Desktop"
   desc "Run the Scripty Discord speech-to-text bot locally with GPU acceleration"
   homepage "https://codeberg.org/robinbraemer/scripty"
